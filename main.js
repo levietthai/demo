09c60250920960b0ce36fc442b8bc56f72cc971a
levietthai <levietthai223@gmail.com>
2024-06-25 14:46:02 +0700

    day 2

---
+++ main.js
@@ -0,0 +1,18 @@
+// const container = document.querySelector('.review-innerr-container');
+// const images = document.querySelectorAll('.review-innerr-container img');
+// const prevButton = document.querySelector('.fa-angle-left');
+// const nextButton = document.querySelector('.fa-chevron-right');
+// const length = images.length
+// let current = 0
+// prevButton.addEventListener('click', () => {
+//     if (current == length - 1) {
+//         current = 0
+//         let width = images[0].offsetWidth
+//         container.style.transform = `translateX(0px)`
+
+//     } else {
+//         current ++
+//         let width = images[0].offsetWidth
+//         container.style.transform = `translateX(${width * -1 * current}px)`
+//     }
+// })
